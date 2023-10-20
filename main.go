9b38c8405d238cf449a477525ad6b34d44be95a2
Mike Miller <chop3593@gmail.com>
2023-10-20 16:25:43 -0600

    First Commit

---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+)
+
+type MessageResponse struct {
+	Message string `json:"message"`
+}
+
+type PodNameResponse struct {
+	PodName string `json:"podName"`
+}
+
+type HealthCheckResponse struct {
+	Status string `json:"status"`
+}
+
+func createResponse(w http.ResponseWriter, data interface{}) {
+	response, err := json.Marshal(data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(response)
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	message := MessageResponse{Message: "Hello World!"}
+	createResponse(w, message)
+}
+
+func messageHandler(w http.ResponseWriter, r *http.Request) {
+	message := MessageResponse{Message: "Yay, K8s!"}
+	createResponse(w, message)
+}
+
+func podNameHandler(w http.ResponseWriter, r *http.Request) {
+	name := os.Getenv("MY_POD_NAME")
+	if name == "" {
+		name = "Not running in K8s!"
+	}
+	podName := PodNameResponse{PodName: name}
+	createResponse(w, podName)
+}
+
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	status := HealthCheckResponse{Status: "OK"}
+	createResponse(w, status)
+}
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/message", messageHandler)
+	http.HandleFunc("/pod_name", podNameHandler)
+	http.HandleFunc("/healthz", healthCheckHandler)
+
+	log.Println("Server listening at http://127.0.0.1:8080/")
+	http.ListenAndServe("127.0.0.1:8080", nil)
+}
---
+++ main_test.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIndexHandler(t *testing.T) {
+	req, err := http.NewRequest("GET", "/", nil)
+	assert.NoError(t, err)
+
+	recorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(indexHandler)
+	handler.ServeHTTP(recorder, req)
+
+	assert.Equal(t, http.StatusOK, recorder.Code)
+
+	var response MessageResponse
+	err = json.Unmarshal(recorder.Body.Bytes(), &response)
+	assert.NoError(t, err)
+	assert.Equal(t, "Hello World!", response.Message)
+}
+
+func TestMessageHandler(t *testing.T) {
+	req, err := http.NewRequest("GET", "/message", nil)
+	assert.NoError(t, err)
+
+	recorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(messageHandler)
+	handler.ServeHTTP(recorder, req)
+
+	assert.Equal(t, http.StatusOK, recorder.Code)
+
+	var response MessageResponse
+	err = json.Unmarshal(recorder.Body.Bytes(), &response)
+	assert.NoError(t, err)
+	assert.Equal(t, "Yay, K8s!", response.Message)
+}
+
+func TestPodNameHandler(t *testing.T) {
+	// Set the MY_POD_NAME environment variable for the test
+	os.Setenv("MY_POD_NAME", "TestPod")
+
+	req, err := http.NewRequest("GET", "/pod_name", nil)
+	assert.NoError(t, err)
+
+	recorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(podNameHandler)
+	handler.ServeHTTP(recorder, req)
+
+	assert.Equal(t, http.StatusOK, recorder.Code)
+
+	var response PodNameResponse
+	err = json.Unmarshal(recorder.Body.Bytes(), &response)
+	assert.NoError(t, err)
+	assert.Equal(t, "TestPod", response.PodName)
+
+	// Clean up the environment variable after the test
+	os.Unsetenv("MY_POD_NAME")
+}
+
+func TestHealthCheckHandler(t *testing.T) {
+	req, err := http.NewRequest("GET", "/healthz", nil)
+	assert.NoError(t, err)
+
+	recorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(healthCheckHandler)
+	handler.ServeHTTP(recorder, req)
+
+	assert.Equal(t, http.StatusOK, recorder.Code)
+
+	var response HealthCheckResponse
+	err = json.Unmarshal(recorder.Body.Bytes(), &response)
+	assert.NoError(t, err)
+	assert.Equal(t, "OK", response.Status)
+}
